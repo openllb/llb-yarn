f0c9fbb5c247472690767ab97e449acca739d733
Edgar Lee <edgarl@netflix.com>
2019-10-23 17:36:34 -0700

    Support yarn workspaces

--- yarn/install.go
+++ yarn/install.go
@@ -2,6 +2,7 @@ package yarn
 
 import (
 	"context"
+	"encoding/json"
 	"path/filepath"
 
 	"github.com/moby/buildkit/client/llb"
@@ -45,14 +46,36 @@ func NewState(ctx context.Context, c client.Client) (*llb.State, error) {
 	inputs := []string{"package.json", "yarn.lock", ".npmrc", ".yarnrc"}
 	src := llb.Local("context",
 		llb.FollowPaths(inputs),
+		llb.WithCustomNamef("load %q", inputs),
 	)
 
-	yarn := llb.Image("docker.io/library/node:alpine")
+	ref := c.BuildOpts().Opts["yarn"]
+	if ref == "" {
+		ref = "docker.io/library/node:alpine"
+	}
+
+	yarn := llb.Image(ref)
 
 	installPath := "/opt/output"
 	yarn = yarn.File(
 		llb.Copy(src, "/", installPath, CopyOptions),
 	)
+
+	patterns, err := WorkspacePatterns(ctx, c, src)
+	if err != nil {
+		return nil, err
+	}
+
+	if len(patterns) > 0 {
+		workspaces := llb.Local("context",
+			llb.IncludePatterns(patterns),
+			llb.WithCustomNamef("load workspaces %q", patterns),
+		)
+
+		yarn = yarn.File(
+			llb.Copy(workspaces, "/", installPath, CopyOptions),
+		)
+	}
 
 	yarn = yarn.Dir(installPath).Run(
 		llb.Shlexf("yarn install --non-interactive --pure-lockfile"),
@@ -69,6 +92,60 @@ func NewState(ctx context.Context, c client.Client) (*llb.State, error) {
 	)
 
 	return &output, nil
+}
+
+func WorkspacePatterns(ctx context.Context, c client.Client, src llb.State) ([]string, error) {
+	caps := c.BuildOpts().LLBCaps
+	marshalOpts := []llb.ConstraintsOpt{llb.WithCaps(caps)}
+
+	def, err := src.Marshal(marshalOpts...)
+	if err != nil {
+		return nil, errors.Wrapf(err, "failed to marshal local source")
+	}
+
+	res, err := c.Solve(ctx, client.SolveRequest{
+		Definition: def.ToPB(),
+	})
+	if err != nil {
+		return nil, errors.Wrapf(err, "failed to resolve dockerfile")
+	}
+
+	ref, err := res.SingleRef()
+	if err != nil {
+		return nil, err
+	}
+
+	content, err := ref.ReadFile(ctx, client.ReadRequest{
+		Filename: "package.json",
+	})
+	if err != nil {
+		return nil, errors.Wrapf(err, "failed to read package.json")
+	}
+
+	var pkg map[string]interface{}
+	err = json.Unmarshal(content, &pkg)
+	if err != nil {
+		return nil, err
+	}
+
+	var patterns []string
+
+	workspacesRaw, ok := pkg["workspaces"]
+	if ok {
+		workspaces, ok := workspacesRaw.([]interface{})
+		if ok {
+			for _, workspaceRaw := range workspaces {
+				workspace, ok := workspaceRaw.(string)
+				if !ok {
+					continue
+				}
+
+				patterns = append(patterns, filepath.Join(workspace, "package.json"))
+			}
+		}
+	}
+
+	return patterns, nil
 }
 
 // ControlCache will force the cache to be ignored id the `--no-cache`
